cfaabb6fa7d27bf20f391c2624a4cc38bf8dd043
QueenBel <bella77bel@gmail.com>
2023-06-08 07:30:38 -0400

    color

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Banner from "../components/Banner";
 
 const Home = () => {
   return (
-    <div className="min-h-[1800px]">
+    <div className="min-h-[1800px] bg-gray-400">
       <Banner />
       <HouseList />
     </div>
